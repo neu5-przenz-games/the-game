71236d247575e325d60df901314ad6df2e14f711
neu5 <neus90@gmail.com>
2021-03-08 17:30:52 +0100

    Create UI draft; resolves #31 (#32)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,13 @@
 module.exports = {
   env: {
-    // allow NodeJs global variables and NodeJS scoping
-    node: true,
-    // allow use of ES6 globals such as Set
+    browser: true,
     es6: true,
     jest: true,
+    node: true,
   },
   extends: ["airbnb-base", "plugin:prettier/recommended"],
   parserOptions: {
-    // allow use of object rest/spread properties as well as other ES8 features
     ecmaVersion: 2018,
-    // allow use of Ecmascript modules
     sourceType: "module",
   },
   rules: {
--- src/game.js
+++ src/game.js
@@ -3,12 +3,25 @@ import GridMovementPlugin from "phaser-grid-movement-plugin";
 
 import GameScene from "./scenes/GameScene";
 
+const windowWidth = window.innerWidth;
+const windowHeight = window.innerHeight;
+
+const gameWidth = windowWidth > 1366 ? 1024 : windowWidth;
+const gameHeight = windowWidth > 1366 ? 768 : windowHeight;
+
+const profileWrapper = document.getElementsByClassName("profile-wrapper")[0];
+const mobileNavBtn = document.getElementsByClassName("mobile-nav-button")[0];
+
+mobileNavBtn.onclick = () => {
+  profileWrapper.classList.toggle("show");
+};
+
 const config = {
   type: Phaser.AUTO,
   scale: {
-    width: 1024,
-    height: 768,
-    autoCenter: Phaser.Scale.CENTER_BOTH,
+    width: gameWidth,
+    height: gameHeight,
+    parent: "the-game",
   },
   plugins: {
     scene: [
