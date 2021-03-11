4855356cf350b6b423aedc312616f333eb3f7e19
neu5 <neus90@gmail.com>
2021-03-11 12:47:33 +0100

    movement

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -14,61 +14,88 @@ export default class Game extends Phaser.Scene {
     this.load.image("tileset", "./assets/tileset/tileset.png");
     this.load.tilemapTiledJSON("map", "./assets/map/map.json");
 
-    this.load.spritesheet("player", "./assets/character/characters.png", {
+    this.load.spritesheet("characters", "./assets/character/characters.png", {
       frameWidth: 52,
       frameHeight: 72,
     });
   }
 
   create() {
-    const tilemap = this.make.tilemap({ key: "map" });
-    const tileset = tilemap.addTilesetImage("tileset", "tileset");
+    this.tilemap = this.make.tilemap({ key: "map" });
+    const tileset = this.tilemap.addTilesetImage("tileset", "tileset");
 
-    tilemap.createLayer("Ground", tileset);
-    tilemap.createLayer("Collisions", tileset);
-    tilemap.createLayer("Above", tileset);
-
-    const playerSprite = this.add.sprite(0, 0, "player");
-    this.cameras.main.startFollow(playerSprite);
-
-    const gridMovementConfig = {
-      characters: [
-        {
-          id: "player",
-          sprite: playerSprite,
-          walkingAnimationMapping: 6,
-          startPosition: new Phaser.Math.Vector2(8, 8),
-        },
-      ],
-    };
-
-    this.gridMovementPlugin.create(tilemap, gridMovementConfig);
+    const groundLayer = this.tilemap.createLayer("Ground", tileset);
+    this.tilemap.createLayer("Collisions", tileset);
+    this.tilemap.createLayer("Above", tileset);
 
     this.socket = io();
-    this.stateStatus = null;
-    this.players = {};
+    this.mainPlayer = null;
+    this.mainPlayerName = null;
+    this.players = [];
 
-    this.socket.on("newPlayer", (player) => {
-      const id = player.playerId;
-      this.playerList.addPlayer(id);
-      this.displayServerMessage(`New player connected! ${id}`);
-      this.players[id] = {
-        playerId: id,
-      };
+    this.cameras.main.setBounds(
+      0,
+      0,
+      this.tilemap.widthInPixels,
+      this.tilemap.heightInPixels
+    );
+
+    this.socket.on("newPlayer", (newPlayer) => {
+      this.displayServerMessage(`New player connected! ${newPlayer.id}`);
     });
     this.socket.on("playerDisconnected", (id) => {
-      this.playerList.removePlayer(id);
+      // this.playerList.removePlayer(id);
       this.displayServerMessage(`Player has left: ${id}`);
-      delete this.players[id];
+      // delete this.players[id];
     });
-    this.socket.on("currentPlayers", (players) => {
+    this.socket.on("currentPlayers", (players, socketId) => {
       this.players = players;
+      this.mainPlayerName = this.players.find(
+        (player) => player.socketId === socketId
+      ).id;
+      this.createPlayers(socketId);
       this.playerList.rebuild(players);
       this.displayServerMessage(`Current players: ${this.playerList.count}`);
     });
+    this.socket.on("playerMoving", (player) => {
+      this.gridMovementPlugin.moveTo(player.id, { x: player.x, y: player.y });
+    });
 
-    this.cameras.main.fadeIn(250);
-    this.stateStatus = "playing";
+    this.input.on(Phaser.Input.Events.POINTER_UP, (pointer) => {
+      const { worldX, worldY } = pointer;
+
+      this.gridMovementPlugin.moveTo(
+        this.mainPlayerName,
+        groundLayer.worldToTileXY(worldX, worldY)
+      );
+    });
+
+    // remember to clean up on Scene shutdown
+    this.events.on(Phaser.Scenes.Events.SHUTDOWN, () => {
+      this.input.off(Phaser.Input.Events.POINTER_UP);
+    });
+  }
+
+  createPlayers(socketId) {
+    this.mainPlayer = this.add.sprite(0, 0, "characters");
+
+    const gridMovementConfig = {
+      characters: this.players.map((player) => {
+        return {
+          id: player.id,
+          sprite:
+            player.socketId === socketId
+              ? this.mainPlayer
+              : this.add.sprite(0, 0, "characters"),
+          walkingAnimationMapping: player.walkingAnimationMapping,
+          facingDirection: player.facingDirection,
+          startPosition: new Phaser.Math.Vector2(player.x, player.y),
+        };
+      }),
+    };
+
+    this.cameras.main.startFollow(this.mainPlayer, true);
+    this.gridMovementPlugin.create(this.tilemap, gridMovementConfig);
   }
 
   displayServerMessage(msgArg) {
@@ -90,17 +117,22 @@ export default class Game extends Phaser.Scene {
     });
   }
 
-  update() {
-    const cursors = this.input.keyboard.createCursorKeys();
+  emitPlayerMovement() {
+    this.socket.emit("playerMovement", {
+      id: this.mainPlayerName,
+      facingDirection: this.gridMovementPlugin.getFacingDirection(
+        this.mainPlayerName
+      ),
+      ...this.gridMovementPlugin.getPosition(this.mainPlayerName),
+    });
+  }
 
-    if (cursors.left.isDown) {
-      this.gridMovementPlugin.moveLeft("player");
-    } else if (cursors.right.isDown) {
-      this.gridMovementPlugin.moveRight("player");
-    } else if (cursors.up.isDown) {
-      this.gridMovementPlugin.moveUp("player");
-    } else if (cursors.down.isDown) {
-      this.gridMovementPlugin.moveDown("player");
+  update() {
+    if (
+      this.mainPlayerName &&
+      this.gridMovementPlugin.isMoving(this.mainPlayerName)
+    ) {
+      this.emitPlayerMovement();
     }
   }
 }
