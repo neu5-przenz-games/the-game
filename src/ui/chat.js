873893d6accc661c4aad94d435914e52272b2510
przenz <przenziu@gmail.com>
2021-03-19 13:06:04 +0100

    Add basic chat room (#46)
    * Add basic chat room
    
    * fix overflow on mobile; resolves #34
    
    * Keep chat always scrolled to the bottom
    
    Co-authored-by: neu5 <neus90@gmail.com>

---
+++ src/ui/chat.js
@@ -0,0 +1,39 @@
+export default class UIChat {
+  constructor(playerName) {
+    this.playerName = playerName;
+    const [chatInput] = document.getElementsByClassName("chat__input");
+    this.chatInput = chatInput;
+    const [chatMessages] = document.getElementsByClassName("chat__messages");
+    this.chatMessages = chatMessages;
+    const [chatContent] = document.getElementsByClassName("chat__content");
+    this.chatContent = chatContent;
+  }
+
+  get message() {
+    return this.chatInput.value;
+  }
+
+  addMessage(name, message, delimeter = ":", classString = null) {
+    const entry = document.createElement("li");
+    if (classString) {
+      entry.classList.add(classString);
+    }
+    entry.appendChild(
+      document.createTextNode(`${name}${delimeter} ${message}`)
+    );
+    this.chatMessages.appendChild(entry);
+    this.chatContent.scrollTop = this.chatContent.scrollHeight;
+  }
+
+  addOwnMessage() {
+    this.addMessage("You", this.message);
+  }
+
+  addServerMessage(message) {
+    this.addMessage("*", message, "", "chat-message__server");
+  }
+
+  clearInput() {
+    this.chatInput.value = "";
+  }
+}
