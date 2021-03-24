5bfbcaaef3837a0c1fa37412235c8f2ba3973542
neu5 <neus90@gmail.com>
2021-03-24 10:30:03 +0100

    Improve extractMap script and generate map at dev/build

--- scripts/extractMap.js
+++ scripts/extractMap.js
@@ -8,14 +8,11 @@ const layerCollides = require("../public/assets/map/map.json").layers.find(
 
 const arr = [];
 
-for (let i = 0; i < layerCollides.width; i += 1) {
-  arr.push([]);
-}
-
 for (let j = 0, m = 0; j < layerCollides.width; j += 1) {
+  arr.push([]);
   for (let k = 0; k < layerCollides.height; k += 1) {
-    const tile = layerCollides.data[m];
-    arr[j][k] = tile === 0 ? -1 : tile;
+    const tileId = layerCollides.data[m];
+    arr[j][k] = tileId === 0 ? -1 : tileId;
     m += 1;
   }
 }
