b0c69869ca7cd895754d4f5046e7937069c788e3
neu5 <neus90@gmail.com>
2021-09-13 10:15:13 +0200

    Implement attack parrying logic #283

--- client/events/sockets.mjs
+++ client/events/sockets.mjs
@@ -230,6 +230,18 @@ export const sockets = (game) => {
     });
   });
 
+  game.socket.on("player:attack-parried", ({ name, message }) => {
+    const player = game.players.get(name);
+
+    TextTween({
+      scene: game,
+      x: player.x,
+      y: player.y,
+      depth: player.depth,
+      message: MESSAGES[message],
+    });
+  });
+
   game.socket.on("player:dead", (name) => {
     const player = game.players.get(name);
 
--- shared/UIMessages/index.mjs
+++ shared/UIMessages/index.mjs
@@ -1,5 +1,6 @@
 const MESSAGES_TYPES = {
   ATTACK_MISSED: "ATTACK_MISSED",
+  ATTACK_PARRIED: "ATTACK_PARRIED",
   NO_ENERGY: "NO_ENERGY",
   NOT_IN_RANGE: "NOT_IN_RANGE",
   NO_RESOURCES: "NO_RESOURCES",
@@ -8,6 +9,7 @@ const MESSAGES_TYPES = {
 
 const MESSAGES = {
   [MESSAGES_TYPES.ATTACK_MISSED]: "Miss",
+  [MESSAGES_TYPES.ATTACK_PARRIED]: "Parried",
   [MESSAGES_TYPES.NO_ENERGY]: "You are exhausted, rest for a while",
   [MESSAGES_TYPES.NOT_IN_RANGE]: "You are too far away, come closer",
   [MESSAGES_TYPES.NO_RESOURCES]: "Not enough resources",
