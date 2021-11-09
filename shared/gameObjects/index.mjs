186019c2314f3a17e8d9eed97434b81abbbc7b85
neu5 <neus90@gmail.com>
2021-11-09 14:45:39 +0100

    Ability to get backpack from LootingBag if player doesn't have any backpack #352 (#363)

--- shared/gameObjects/index.mjs
+++ shared/gameObjects/index.mjs
@@ -1,7 +1,21 @@
 import { CopperOre } from "./CopperOre.mjs";
 import { HealingStone } from "./HealingStone.mjs";
 import { House } from "./House.mjs";
-import { LootingBag, mergeItems } from "./LootingBag.mjs";
+import {
+  LootingBag,
+  getNewLootingBagItems,
+  mergeItems,
+  removeItemsFromLootingBag,
+} from "./LootingBag.mjs";
 import { Tree } from "./Tree.mjs";
 
-export { CopperOre, HealingStone, House, LootingBag, Tree, mergeItems };
+export {
+  CopperOre,
+  HealingStone,
+  House,
+  LootingBag,
+  Tree,
+  getNewLootingBagItems,
+  mergeItems,
+  removeItemsFromLootingBag,
+};
