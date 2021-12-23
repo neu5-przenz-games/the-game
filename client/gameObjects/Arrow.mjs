accbdbeb7318e8f5106fc04fef532389e757af1f
neu5 <neus90@gmail.com>
2021-12-23 14:12:48 +0100

    Fix arrows visibility #408 (#409)

--- client/gameObjects/Arrow.mjs
+++ client/gameObjects/Arrow.mjs
@@ -4,6 +4,7 @@ export class Arrow extends Phaser.GameObjects.Image {
   constructor(scene, x, y) {
     super(scene, x, y, "arrow-bow");
 
+    this.setDepth(Infinity);
     this.setScale(0.5, 0.5);
     this.visible = false;
   }
